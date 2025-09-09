5d5e8a4d720230a6dc1d1b8a563a91196d54d257
dongesz <majormate12@gmail.com>
2025-09-09 11:47:31 +0200

    feat: feladat5 - done
    - magyar-szavak.txt -> szotar.txt, csak ertelmes szavak.
    - otimalis, optimalizalatlan megoldas

---
+++ 2025.09.09/Feladatok/feladat5.cs
@@ -0,0 +1,38 @@
+﻿namespace feladat5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "magyar-szavak.txt";
+            string[] sorok = File.ReadAllLines(path);
+            //feladat1 - optimalizalatlan
+            /*
+            File.WriteAllText("szotar.txt", "");
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                bool csakbetuk = true;
+                foreach (var item in sorok[i])
+                {
+                 if(!char.IsLetter(item)) csakbetuk = false;
+                }
+                if (sorok[i].ToLower() == sorok[i] && csakbetuk) File.AppendAllText("szotar.txt",sorok[i] + "\n");
+            }
+            Console.WriteLine("kesz");*/
+
+            //feladat1 - optimalis
+            List<string> szotar = new List<string>();
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                bool csakbetuk = true;
+                foreach (var item in sorok[i])
+                {
+                    if (!char.IsLetter(item)) csakbetuk = false;
+                }
+                if (sorok[i].ToLower() == sorok[i] && csakbetuk) szotar.Add(sorok[i]);
+            }
+            File.AppendAllLines("szotar.txt", szotar);
+            Console.WriteLine("kesz");
+        }
+    }
+}
