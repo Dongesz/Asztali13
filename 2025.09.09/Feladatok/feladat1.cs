c0e5a76eb5e4eadd4dcba6e84f801912b7555d1e
dongesz <majormate12@gmail.com>
2025-09-09 09:00:26 +0200

    feat: feladat1 - done
    - szamok2.txt letrehozasa
    - szamok2.txt feltoltese 0-1000 ig szamokkal kettesevel novekedve
    - szamok2.txt tartalmanak beolvasasa is kiirasa

---
+++ 2025.09.09/Feladatok/feladat1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Feladat1
+            string path = "szamok.txt";
+            if (File.Exists(path)) File.Delete(path);
+
+            for (int i = 0; i <= 1000; i += 2)
+            {
+                if (File.Exists(path))
+                {
+                    File.AppendAllText(path, i.ToString());
+                    File.AppendAllText(path, ",");
+
+                }
+                else
+                {
+                    File.WriteAllText(path, i.ToString());
+                    File.AppendAllText(path, ",");
+                }
+            }
+
+            string[] szamok = File.ReadAllLines(path);
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine($"{szamok[i]}".TrimEnd(','));
+            }
+        }
+    }
+}
