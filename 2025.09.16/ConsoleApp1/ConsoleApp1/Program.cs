7b6a4b0a182b7bd311110dfb6c5d6926937ef4d3
dongesz <majormate12@gmail.com>
2025-09-16 07:43:41 +0200

    chore(start) - Class Program

---
+++ 2025.09.16/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        class Szemely
+        {
+            private string _name;
+            private int _weight;
+            private bool _dLicense;
+
+            public Szemely(string name, int weight, bool dLicense)
+            {
+                _name = name;
+                _weight = weight;
+                _dLicense = dLicense;
+            }
+
+            public override string ToString()
+            {
+                return $"Név: {_name}, Súly: {_weight}kg,  Jogosítvány: {(_dLicense?"Van":"Nincs")}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Szemely szemely1 = new Szemely("Peti", 55, true);
+            Console.WriteLine(szemely1.ToString());
+        }
+    }
+}
