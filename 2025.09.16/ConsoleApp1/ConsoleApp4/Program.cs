265b9b1c853d2689eda1660a15e6e94ff5447794
dongesz <majormate12@gmail.com>
2025-09-16 12:11:34 +0200

    chore(Final): remove unecessery code

--- 2025.09.16/ConsoleApp1/ConsoleApp4/Program.cs
+++ 2025.09.16/ConsoleApp1/ConsoleApp4/Program.cs
@@ -28,11 +28,12 @@ namespace ConsoleApp4
 
             public override string ToString()
             {
-                return $"Tanar neve: {_nev}, Eletkora: {_kor}, szakmaja: {_szak}, heti oraszama: {_oraszam}, osztalyfonok: {(_osztalyfonok ? "igen" : "nem")}, fizetese: {_fizetes}";
+                return $"Tanar neve: {_nev}, Eletkora: {_kor}, szakmaja: {_szak}, heti oraszama: {_oraszam}, osztalyfonok: {(_osztalyfonok ? "igen" : "nem")}, fizetese: {_fizetes}ft";
             }
             public void FizetesEmeles(Tanar tanar, int emeles)
             {
-                tanar._fizetes += emeles;
+                if(emeles > 0)
+                    tanar._fizetes += emeles;
             }
             public void OratAd(Tanar tanar, Osztaly osztaly)
             {
@@ -55,16 +56,18 @@ namespace ConsoleApp4
                 _list = list;
             }
 
-            public void Felvesz(Tanar tanar)
+            public void Felvesz(Tanar tanar) // Tanar felvetele
             {
-                _list.Add(tanar);
+                if (tanar != null)
+                    _list.Add(tanar);
             }
-            public void Kirug(Tanar tanar)
+            public void Kirug(Tanar tanar) // Tanar eltavolitasa
             {
-                _list.Remove(tanar);
+                if (tanar != null)
+                    _list.Remove(tanar);
             }
             
-            public void Tanari()
+            public void Tanari() // Tanarok kilistazasa
             {
                 foreach(Tanar tanar in _list)
                 {
@@ -92,11 +95,12 @@ namespace ConsoleApp4
             {
                 return $"Diak neve: {_nev}, Eletkora: {_kor}, allapota: {_elmeAllapot}, osztondij: {_osztondij}"; 
             }
-            public void OsztondijEmeles(Diak diak, int emeles)
+            public void OsztondijEmeles( int emeles)
             {
-                diak._osztondij += emeles;
+                if( emeles > 0)
+                    this._osztondij += emeles;
             }
-            public void FigyelE(Diak diak)
+            public void FigyelE() // megallapitja hogy a diak figyel vagy sem
             {
                 if (_elmeAllapot < 50)
                 {
@@ -108,29 +112,40 @@ namespace ConsoleApp4
         }
         public class Osztaly
         {
-            public List<Diak> _diakok = new List<Diak>();
-            const int _osztalylimit = 30;
+            public List<Diak> _diakok = new List<Diak>(); // Az osszes diakot tarolja
             public string _szak;
+            public string _osztalyfonok;
 
-            public Osztaly(List<Diak> list, string szak)
+            public Osztaly(List<Diak> list, string szak, string osztalyfonok)
             {
                 _diakok = list;
                 _szak = szak;
+                _osztalyfonok = osztalyfonok;
             }
 
-            public void felvesz(Diak diak)
+            public void felvesz(Diak diak) // Diak hozzaadasa
             {
-                _diakok.Add(diak);
+                if (diak != null)
+                    _diakok.Add(diak);
             }
-            public void Ekltanacsol(Diak diak)
+            public void Ekltanacsol(Diak diak) // Diak eltavolitasa 
             {
-                _diakok.Remove(diak);
+                if (diak != null)
+                    _diakok.Remove(diak);
             }
-            public void Osztalyterem()
+            public void Osztalyterem() // Osztaly adatok kilistazasa
             {
-                foreach(Diak diak in _diakok)
+                if (_diakok.Count == 0)
                 {
-                    Console.WriteLine(diak);
+                    Console.WriteLine("Üres az osztaly!");
+                }
+                else
+                {
+                    Console.WriteLine($"Osztalyfonok: {_osztalyfonok}");
+                    foreach (Diak diak in _diakok)
+                    {
+                        Console.WriteLine(diak);
+                    }
                 }
             }
         }
@@ -146,8 +161,8 @@ namespace ConsoleApp4
             // Osztályok inicializálása
             List<Diak> informatikaDiakok = new List<Diak> { diak1, diak2, diak4 };
             List<Diak> matematikaDiakok = new List<Diak> { diak3 };
-            Osztaly tizenharmadikB = new Osztaly(informatikaDiakok, "Informatika");
-            Osztaly tizenharmadikA = new Osztaly(matematikaDiakok, "Matematika");
+            Osztaly tizenharmadikB = new Osztaly(informatikaDiakok, "Informatika", "Deak Csaba");
+            Osztaly tizenharmadikA = new Osztaly(matematikaDiakok, "Matematika", "Farkas zoltan");
 
             // Tanárok
             Tanar tanar1 = new Tanar("Deák Csaba", 30, "Informatika", 20, true, 300000, 10);
@@ -161,69 +176,53 @@ namespace ConsoleApp4
             // Tesztelés kezdete
             Console.WriteLine("--- Teszt 1: Osztályterem kiíratása (tizenharmadikB) ---");
             tizenharmadikB.Osztalyterem();
-            // Várható kimenet:
-            // Diak neve: Kovács Anna, Eletkora: 16, allapota: 80, osztondij: 50000
-            // Diak neve: Nagy Péter, Eletkora: 17, allapota: 90, osztondij: 60000
-            // Diak neve: Tóth Bence, Eletkora: 17, allapota: 40, osztondij: 40000
+            
 
             Console.WriteLine("\n--- Teszt 2: Tanárok kiíratása ---");
             tanarok.Tanari();
-            // Várható kimenet:
-            // Tanar neve: Deák Csaba, Eletkora: 30, szakmaja: Informatika, heti oraszama: 20, osztalyfonok: igen, fizetese: 300000
-            // Tanar neve: Farkas Zoltán, Eletkora: 32, szakmaja: Matematika, heti oraszama: 15, osztalyfonok: nem, fizetese: 280000
-
+           
             Console.WriteLine("\n--- Teszt 3: Fizetésemelés tanar1 számára ---");
             tanar1.FizetesEmeles(tanar1, 20000);
             Console.WriteLine(tanar1.ToString());
-            // Várható kimenet: Tanar neve: Deák Csaba, Eletkora: 30, ..., fizetese: 320000
 
             Console.WriteLine("\n--- Teszt 4: Ösztöndíjemelés diak2 számára ---");
-            diak2.OsztondijEmeles(diak2, 10000);
+            diak2.OsztondijEmeles(10000);
             Console.WriteLine(diak2.ToString());
-            // Várható kimenet: Diak neve: Nagy Péter, Eletkora: 17, allapota: 90, osztondij: 70000
 
             Console.WriteLine("\n--- Teszt 5: Tanár óraadás (sikeres eset) ---");
             tanar1.OratAd(tanar1, tizenharmadikB);
             tizenharmadikB.Osztalyterem();
-            // Várható kimenet (minden diák _elmeAllapot értéke csökken 10-zel):
-            // Diak neve: Kovács Anna, Eletkora: 16, allapota: 70, osztondij: 50000
-            // Diak neve: Nagy Péter, Eletkora: 17, allapota: 80, osztondij: 70000
-            // Diak neve: Tóth Bence, Eletkora: 17, allapota: 30, osztondij: 40000
+
 
             Console.WriteLine("\n--- Teszt 6: Tanár óraadás (sikertelen eset, rossz szak) ---");
             tanar2.OratAd(tanar2, tizenharmadikB);
-            // Várható kimenet: "A Szaktanarnak nem itt van oraja!"
 
             Console.WriteLine("\n--- Teszt 7: Diák felvétele és eltávolítása ---");
             tizenharmadikA.felvesz(diak4);
             tizenharmadikA.Osztalyterem();
-            // Várható kimenet:
-            // Diak neve: Szabó Eszter, Eletkora: 16, allapota: 70, osztondij: 45000
-            // Diak neve: Tóth Bence, Eletkora: 17, allapota: 30, osztondij: 40000
+
             tizenharmadikA.Ekltanacsol(diak4);
             Console.WriteLine("Diák eltávolítása után:");
             tizenharmadikA.Osztalyterem();
-            // Várható kimenet:
-            // Diak neve: Szabó Eszter, Eletkora: 16, allapota: 70, osztondij: 45000
+
 
             Console.WriteLine("\n--- Teszt 8: Tanár felvétele és kirúgása ---");
             tanarok.Felvesz(tanar3);
             tanarok.Tanari();
-            // Várható kimenet: mindhárom tanár adatai
+       
             tanarok.Kirug(tanar3);
             Console.WriteLine("Tanár kirúgása után:");
             tanarok.Tanari();
-            // Várható kimenet: csak tanar1 és tanar2 adatai
+
 
             Console.WriteLine("\n--- Teszt 9: Edge case - üres osztály ---");
-            Osztaly uresOsztaly = new Osztaly(new List<Diak>(), "Fizika");
+            Osztaly uresOsztaly = new Osztaly(new List<Diak>(), "Fizika", "Negyesi Gabor");
             uresOsztaly.Osztalyterem();
-            // Várható kimenet: üres, nem ír ki semmit
+
 
             Console.WriteLine("\n--- Teszt 10: FigyelE metódus tesztelése ---");
-            diak1.FigyelE(diak1); // elmeAllapot: 70, várható: semmi, mert >= 50
-            diak4.FigyelE(diak4); // elmeAllapot: 30, várható: "Tóth Bence elkalandozott"
-
+            diak1.FigyelE();
+            diak4.FigyelE(); 
         }
     }
 }
