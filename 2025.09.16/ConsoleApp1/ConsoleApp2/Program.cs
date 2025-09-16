09b96334cde3323cac4f4588f203047ef678e0d8
dongesz <majormate12@gmail.com>
2025-09-16 08:11:37 +0200

    feat: Konyv, Konyvesbolt class
    - initialize konyv class
    	- create field variables
    	- create constructor
    	- create tostring override
    	- create antik method - checks if a book is antik or not
    - initialize KonyvBolt class
    	- initialize List<Konyv> field variable
    	- create Add method - Add books to list
    	- create Remove method - Remove books from list
    	- create Show method - output list elements

--- 2025.09.16/ConsoleApp1/ConsoleApp2/Program.cs
+++ 2025.09.16/ConsoleApp1/ConsoleApp2/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace ConsoleApp2
+﻿using System.ComponentModel;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
+        public class Konyv
+        {
+            private string _author;
+            private string _title;
+            private int _age;
+            private int _value;
+
+            public Konyv(string author, string title, int age, int value)
+            {
+                _author = author;
+                _title = title;
+                _age = age;
+                _value = value;
+            }
+            public override string ToString()
+            {
+                return $"Szerzo: {_author}, Cim: {_title}, Ev: {_age}, Ar: {_value}";
+            }
+            public bool antik()
+            {
+                if (this._age <= 1950) return true ;
+                else return false ;
+            }
+            public class KonyvesBolt
+            {
+                private List<Konyv> _antikKonyvek;
+
+                public void Add(Konyv konyv)
+                {
+                    if (konyv.antik())
+                    {
+                        _antikKonyvek.Add(konyv);
+                    }
+                    else Console.WriteLine("Csak antik könyveket vasarlunk!");      
+                }
+                public void Remove(Konyv konyv)
+                {
+                    _antikKonyvek.Remove(konyv);
+                }
+                public void Show(Konyv konyv)
+                {
+                    foreach (var item in _antikKonyvek)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
