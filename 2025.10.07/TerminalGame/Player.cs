8e914c7848299f284c5f77c3b783b6bf43ac91b8
Dongesz <majormate12@gmail.com>
2025-10-06 19:56:50 +0200

    feat(Console): Initialized Base classes
    - Initialized base classes: Enemy, Player

---
+++ 2025.10.07/TerminalGame/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalGame
+{
+    internal class Player
+    {
+        private string Name { get; set; }
+        public int Hp { get; set; }
+        private int Dp { get; set; }
+        private int Defense { get; set; }
+
+        private readonly int _initialHp;
+        private readonly int _initialDp;
+        private readonly int _initialDefense;
+
+        public Player(string name, int hp, int dp, int defense)
+        {
+            Name = name;
+            Hp = hp;
+            Dp = dp;
+            Defense = defense;
+
+            _initialHp = hp;
+            _initialDp = dp;
+            _initialDefense = defense;
+        }
+
+        public void takeDamage(int Dp)
+        {
+            this.Hp -= Dp;
+        }
+
+        public void attack(Enemy enemy)
+        {
+            enemy.Hp -= Dp;
+        }
+
+        public void resetStats()
+        {
+            Hp = _initialHp;
+            Dp = _initialDp;
+            Defense = _initialDefense;
+        }
+    }
+}
