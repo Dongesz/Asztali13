cc320a21eeeb8b48b2cb265a8061b4bc55a13870
Dongesz <majormate12@gmail.com>
2025-10-06 20:54:46 +0200

    feat(Console): Implement stat save

---
+++ 2025.10.07/TerminalGame/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerminalGame
+{
+    internal static class FileManager
+    {
+        public static void SavePlayerStats(Player player, int rounds, int wins, string fileName = "player_stats.txt")
+        {
+            string content =
+                $"Date: {DateTime.Now}\n" +
+                $"Name: {player.Name}\n" +
+                $"Class: {player.GetType().Name}\n" +
+                $"HP: {player.Hp}, DP: {player.Dp}, Defense: {player.Defense}\n" +
+                $"Rounds Played: {rounds}, Wins: {wins}\n" +
+                $"-----------------------------\n";
+            File.AppendAllText(fileName, content);
+        }
+
+        public static string ReadAllStats(string fileName = "player_stats.txt")
+        {
+            return File.Exists(fileName) ? File.ReadAllText(fileName) : "No stats found.";
+        }
+    }
+}
