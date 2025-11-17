b5a1e726908af072fd4da48000cb770948ed5592
Dongesz <majormate12@gmail.com>
2025-11-18 00:34:48 +0100

    feat: 2025.11.18 - villogo gomb jatek v1.1

---
+++ 2025.11.18/WpfApp1/Gomb.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace WpfApp1
+{
+   
+        public class Gomb : Button
+        {
+            private Random random = new Random();
+            public ButtonState CurrentState { get; private set; } = ButtonState.Inactive;
+
+            public Gomb()
+            {
+                Width = 80;
+                Height = 80;
+                Margin = new Thickness(5);
+                ResetButtonState();
+            }
+
+            public virtual void Activate()
+            {
+                CurrentState = ButtonState.Green;
+                Background = Brushes.Green;
+            }
+
+            public virtual void ChangeColor()
+            {
+                switch (CurrentState)
+                {
+                    case ButtonState.Green:
+                        CurrentState = ButtonState.Yellow;
+                        Background = Brushes.Yellow;
+                        break;
+                    case ButtonState.Yellow:
+                        CurrentState = ButtonState.Red;
+                        Background = Brushes.Red;
+                        break;
+                    case ButtonState.Red:
+                        CurrentState = ButtonState.Inactive;
+                        Background = Brushes.Gray;
+                        break;
+            }
+            }
+
+            public virtual int CalculateScore()
+            {
+                
+                switch(CurrentState)
+                {
+                    case ButtonState.Green:
+                        return 10;
+                    case ButtonState.Yellow:
+                        return 5;
+                    default:
+                        return 0;
+                }
+
+            }
+
+            public virtual void ResetButtonState()
+            {
+                CurrentState = ButtonState.Inactive;
+                Background = Brushes.Gray;
+            }
+
+            public void ApplyTemplate(Window window)
+            {
+                Template = (ControlTemplate)window.FindResource("NoMouseOverButtonTemplate");
+            }
+    }
+}
+
---
+++ 2025.11.18/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int score = 0;
+        private int highscore = 0;
+        private Random random = new Random();
+        private DispatcherTimer timer;
+        private Gomb currentActiveButton;
+        private double difficulty = 2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeGame()
+        {
+            GameGrid.Children.Clear();
+            
+
+            for (int i = 0; i < 9; i++)
+            {
+                var button = new Gomb();
+                button.Click += Button_Click;
+                button.ApplyTemplate(this);
+                GameGrid.Children.Add(button);
+            }
+            ActivateRandomButton();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Gomb button && button == currentActiveButton)
+            {
+                score += button.CalculateScore();
+                if (highscore < score)
+                {
+                    highscore = score;
+                    HighScore.Text = $"High Score: {highscore}";
+                }
+                ScoreText.Text = $"Score: {score}";
+                button.ResetButtonState();
+                currentActiveButton = null;
+                ActivateRandomButton();
+            }
+        }
+
+        private void StartGameTimer()
+        {
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(difficulty)
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (currentActiveButton != null)
+            {
+                currentActiveButton.ChangeColor();
+                //currentActiveButton.Content = "" + currentActiveButton.CurrentState;
+                if(currentActiveButton.CurrentState==ButtonState.Red)
+                {
+                    score -= 5;
+                    ScoreText.Text = $"Score: {score}";
+                }else
+                if (currentActiveButton.CurrentState == ButtonState.Inactive)
+                {
+                    currentActiveButton.ResetButtonState();
+                    currentActiveButton = null;
+                   
+                    ActivateRandomButton();
+                }
+                EndGame();
+                
+            }
+        }
+
+        private void ActivateRandomButton()
+        {
+            if (currentActiveButton == null)
+            {
+                int index = random.Next(0, GameGrid.Children.Count);
+                currentActiveButton = (Gomb)GameGrid.Children[index];
+                currentActiveButton.Activate();
+            }
+        }
+
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            startgomb.Visibility = Visibility.Hidden;
+            tittle.Visibility = Visibility.Hidden;
+            diffslider.Visibility = Visibility.Hidden;
+            difftxt.Visibility = Visibility.Hidden;
+            ScoreText.Visibility = Visibility.Visible;
+            GameGrid.Visibility = Visibility.Visible;
+
+            InitializeGame();
+            StartGameTimer();
+        }
+        private void EndGame()
+        {
+            if (score < 0)
+            {
+                ResetGame();
+                ScoreText.Visibility = Visibility.Hidden;
+                GameGrid.Visibility = Visibility.Hidden;
+                startgomb.Visibility = Visibility.Visible;
+                tittle.Visibility = Visibility.Visible;
+                diffslider.Visibility = Visibility.Visible;
+                difftxt.Visibility = Visibility.Visible;
+            }
+        }
+       
+        private void Difficulty(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            difficulty = diffslider.Value;
+        }
+
+        private void ResetGame()
+        {
+            if (currentActiveButton != null)
+            {
+                currentActiveButton.ResetButtonState();
+                currentActiveButton = null;
+            }
+
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Tick -= Timer_Tick;
+                timer = null;
+            }
+
+            score = 0;
+            GameGrid.Children.Clear();
+            ScoreText.Text = $"Score: {score}";
+        }
+
+    }
+
+    public enum ButtonState
+    {
+        Inactive,
+        Green,
+        Yellow,
+        Red
+    }
+
+    
+}
