e8a4c9d6a402088f32899a8c8b55f10357510bc7
Dongesz <majormate12@gmail.com>
2025-12-08 18:39:44 +0100

    chore: initial commit for films app

---
+++ 2025.12.09/WpfApp1/Film.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Film
+    {
+        public string Cim {  get; set; }
+        public string Mufaj { get; set; }
+        public int Hossz { get; set; }
+        public double Ertekeles { get; set; }
+
+        public Film(string cim, string mufaj, int hossz, double ertekeles)
+        {
+            Cim = cim;
+            Mufaj = mufaj;
+            Hossz = hossz;
+            Ertekeles = ertekeles;
+        }
+    }
+}
---
+++ 2025.12.09/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Film> filmek = new List<Film>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FilmekBetoltese(object sender, RoutedEventArgs e)
+        {
+            filmek.Clear();
+            filmek.Add(new Film("Csillagok haboruja", "sci-fi", 121, 8.6));
+        }
+    }
+}
